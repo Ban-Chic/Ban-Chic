f6a42b8ad4fbd674925cb23ac671f84628191477
HJ-dp <skwo777@naver.com>
2024-03-18 14:51:22 +0900

    build: msw 설정
    #S10P22B109-151

--- front/src/mocks/handlers.js
+++ front/src/mocks/handlers.js
@@ -1,14 +1,13 @@
-import { rest } from "msw";
-
-const todos = ["먹기", "자기", "놀기"];
+import { http } from "msw";
 
 export const handlers = [
-  // 할일 목록
-  rest.get("/todos", (req, res, ctx) => res(ctx.status(200), ctx.json(todos))),
-
-  // 할일 추가
-  rest.post("/todos", (req, res, ctx) => {
-    todos.push(req.body);
-    return res(ctx.status(201));
+  http.get("/posts", () => {
+    console.log('Captured a "GET /posts" request');
+  }),
+  http.post("/posts", () => {
+    console.log('Captured a "POST /posts" request');
+  }),
+  http.delete("/posts/:id", ({ params }) => {
+    console.log(`Captured a "DELETE /posts/${params.id}" request`);
   }),
 ];
