fe6b7994aefd560308763a56191ff8bd8393b790
이호진 <skwo777@naver.com>
2024-03-21 13:29:01 +0900

    style: font, color 설정

--- front/src/styles/Color.js
+++ front/src/styles/Color.js
@@ -2,6 +2,7 @@ const color = {
   // 백그라운드 컬러
   bgColor: "#191919",
   fontColor: "#f2f2f2",
+  sectionColor: "#6c6c6c",
 };
 
 export default color;
---
+++ front/src/styles/Style.js
@@ -0,0 +1,10 @@
+const styleBase = {
+  glassmorphism: `
+  background: rgba( 255, 255, 255, 0.4 );
+  box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+  backdrop-filter: blur( 1px );
+  -webkit-backdrop-filter: blur( 0.5px );
+  border: 1px solid rgba( 255, 255, 255, 0.18 );`,
+};
+
+export default styleBase;
