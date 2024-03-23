ae1d6ee4d622fddbd841fec933ca3631426bb834
NavyHubb <sngjae972@gmail.com>
2024-03-23 23:50:37 +0900

    feat: FileUploadService 개발
    [- S3 파일 업로드 관련 컴포넌트 추가]

--- back/src/main/java/com/ssafy/banchic/exception/ErrorCode.java
+++ back/src/main/java/com/ssafy/banchic/exception/ErrorCode.java
@@ -8,7 +8,11 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
 
-    NOT_FOUND_ID(HttpStatus.BAD_REQUEST, "해당 아이디를 찾을 수 없습니다.");
+    NOT_FOUND_ID(HttpStatus.BAD_REQUEST, "해당 아이디를 찾을 수 없습니다."),
+
+    FILE_UPLOAD_FAIL(HttpStatus.BAD_REQUEST, "파일 업로드에 실패했습니다."),
+    FILE_DELETE_FAIL(HttpStatus.BAD_REQUEST, "파일 삭제에 실패했습니다."),
+    NOT_VALID_EXTENSION(HttpStatus.BAD_REQUEST, "유효하지 않은 확장자입니다.");
 
     private final HttpStatus httpStatus;
     private final String detail;
