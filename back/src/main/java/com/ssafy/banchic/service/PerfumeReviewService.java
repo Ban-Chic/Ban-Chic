bcc4566d1daf78932336f54c8d89e91eaa857cb0
김성재 <sngjae972@gmail.com>
2024-03-25 21:16:06 +0900

    feat: 리뷰 작성 기능 개발

--- back/src/main/java/com/ssafy/banchic/service/PerfumeReviewService.java
+++ back/src/main/java/com/ssafy/banchic/service/PerfumeReviewService.java
@@ -1,17 +1,23 @@
 package com.ssafy.banchic.service;
 
+import com.ssafy.banchic.domain.dto.request.ReviewReq;
 import com.ssafy.banchic.domain.dto.response.ReviewRes;
+import com.ssafy.banchic.domain.entity.Member;
 import com.ssafy.banchic.domain.entity.Perfume;
 import com.ssafy.banchic.domain.entity.Review;
 import com.ssafy.banchic.exception.CustomException;
 import com.ssafy.banchic.exception.ErrorCode;
+import com.ssafy.banchic.repository.MemberRepository;
 import com.ssafy.banchic.repository.PerfumeRepository;
 import com.ssafy.banchic.repository.PerfumeReviewRepository;
+import com.ssafy.banchic.util.TokenProvider;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +26,9 @@ public class PerfumeReviewService {
 
     private final PerfumeRepository perfumeRepository;
     private final PerfumeReviewRepository perfumeReviewRepository;
+    private final MemberRepository memberRepository;
+    private final FileUploadService fileUploadService;
+    private final TokenProvider tokenProvider;
 
     @Transactional(readOnly = true)
     public Page<ReviewRes> getList(Long perfumeId, Pageable pageable) {
@@ -27,6 +36,29 @@ public class PerfumeReviewService {
             .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_ID));
         Page<Review> reviews = perfumeReviewRepository.getReviewsByPerfume(perfume, pageable);
         return reviews.map(ReviewRes::from);
+    }
+
+    public Long create(Long perfumeId, ReviewReq reviewReq, MultipartFile file,HttpServletRequest httpServletRequest) {
+        Perfume perfume = perfumeRepository.findById(perfumeId)
+            .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_ID));
+
+        Member member = getMemberFromAccessToken(httpServletRequest);
+
+        Review review = Review.builder()
+            .rate(reviewReq.getRate())
+            .content(reviewReq.getContent())
+            .imgUrl(fileUploadService.save("review/", file))
+            .perfume(perfume)
+            .member(member)
+            .build();
+
+        return perfumeReviewRepository.save(review).getId();
+    }
+
+    public Member getMemberFromAccessToken(HttpServletRequest request) {
+        Member memberFromAccessToken = tokenProvider.getMemberFromAccessToken(request);
+        return memberRepository.findById(memberFromAccessToken.getId())
+            .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_ID));
     }
 
 }
