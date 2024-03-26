6471488b1b318cd2fc60facd2d45815935ac1454
HYUNJUN, KIM <denny10002@naver.com>
2024-03-27 00:24:27 +0900

    feat : csv to rdb test

--- back/src/main/java/com/ssafy/banchic/config/dummy/DataInitalizationRunner.java
+++ back/src/main/java/com/ssafy/banchic/config/dummy/DataInitalizationRunner.java
@@ -5,18 +5,22 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class DataInitalizationRunner implements ApplicationRunner {
 
     private final DummyDataService dummyDataService;
+    private final CsvDataService csvDataService;
 
-    public DataInitalizationRunner(DummyDataService dummyDataService) {
+    public DataInitalizationRunner(DummyDataService dummyDataService, CsvDataService csvDataService) {
         this.dummyDataService = dummyDataService;
+        this.csvDataService = csvDataService;
     }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         // 애플리케이션 초기화 시점에서 DummyData 실행
-        dummyDataService.insertDummyData();
+//        dummyDataService.insertDummyData();
+        csvDataService.importCsvData("C:\\Users\\denny\\Downloads\\finalPerfume.CSV");
+
     }
 }
