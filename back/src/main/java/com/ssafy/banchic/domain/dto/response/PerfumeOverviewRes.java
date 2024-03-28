75864713965df115a4f068ff4044f34ba2226da3
김성재 <sngjae972@gmail.com>
2024-03-29 00:26:59 +0900

    refactor: 추천 향수 API  반환 시 포함되는 컬럼 추가

--- back/src/main/java/com/ssafy/banchic/domain/dto/response/PerfumeOverviewRes.java
+++ back/src/main/java/com/ssafy/banchic/domain/dto/response/PerfumeOverviewRes.java
@@ -11,13 +11,19 @@ public class PerfumeOverviewRes {
     private Integer id;
     private String perfumeName;
     private String perfumeImg;
+    private String brandName;
+    private int year;
+    private String accords;
 
     public static PerfumeOverviewRes from(Perfume perfume) {
         return PerfumeOverviewRes.builder()
-                .id(perfume.getId())
-                .perfumeName(perfume.getPerfumeName())
-                .perfumeImg(perfume.getPerfumeImg())
-                .build();
+            .id(perfume.getId())
+            .perfumeName(perfume.getPerfumeName())
+            .perfumeImg(perfume.getPerfumeImg())
+            .brandName(perfume.getBrandName())
+            .year(perfume.getYear())
+            .accords(perfume.getAccords())
+            .build();
     }
 
 }
