a63929eb9d8b0d813bf536a68a270605a4b4383d
김성재 <sngjae972@gmail.com>
2024-04-01 11:03:36 +0900

    refactor: SecurityConfig에서의 CORS 설정 삭제

--- back/src/main/java/com/ssafy/banchic/config/WebConfig.java
+++ back/src/main/java/com/ssafy/banchic/config/WebConfig.java
@@ -1,18 +1,18 @@
-//package com.ssafy.banchic.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//            .allowedOrigins("http://j10b109.p.ssafy.io:5173")
-//            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//            .exposedHeaders("Authorization", "RefreshToken");
-//    }
-//
-//}
+package com.ssafy.banchic.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://j10b109.p.ssafy.io:5173")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .exposedHeaders("Authorization", "RefreshToken");
+    }
+
+}
