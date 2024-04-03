5db1841882efd30bfdb4916cb4663f9e3d41262f
구본민 <gugabm@naver.com>
2024-04-04 02:25:21 +0900

    refactor: gpt align

--- front/src/components/molecules/gptApi/gptWriter.tsx
+++ front/src/components/molecules/gptApi/gptWriter.tsx
@@ -46,6 +46,9 @@ const SContainer = styled.div`
   width: 100%;
   height: 100%;
   padding: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const SDescription = styled.div`
