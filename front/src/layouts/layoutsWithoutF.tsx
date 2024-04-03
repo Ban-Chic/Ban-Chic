74fca8ee4b0134602eace5ca3c71866f9056fcf2
구본민 <gugabm@naver.com>
2024-04-03 21:19:06 +0900

    refactor: review all fixed

---
+++ front/src/layouts/layoutsWithoutF.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+import GNB from "../components/molecules/common/gnb";
+import { Outlet } from "react-router";
+
+function Layouts() {
+  return (
+    <SLayout>
+      <GNB />
+      <main>
+        <Outlet />
+      </main>
+    </SLayout>
+  );
+}
+
+const SLayout = styled.div`
+  width: 100%;
+
+  &:nth-child(1) > :nth-child(2) {
+    min-height: calc(100vh - (250px));
+  }
+`;
+
+export default Layouts;
